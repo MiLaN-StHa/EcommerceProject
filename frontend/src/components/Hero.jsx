60f515b4f2179563d80602e14249a31a95ff652b
Milan <xthamilan88@gmail.com>
2025-02-03 13:36:03 +0545

    latest colletction section created

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/frontend_assets/assets'
-import homeimg from '../assets/heroimg.jpg'
+import homeimg from '../assets/hero.jpg'
 const Hero = () => {
   return (
     <div className='flex flex-col sm:flex-row border border-gray-400'>
@@ -9,13 +9,12 @@ const Hero = () => {
             <div className='text-[#414141]'>
                 <div className='flex items-center gap-2'>
                     <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                    <p className='font-medium text-sm md:text-base'>BESTSELLERS</p>
+                    <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
                 </div>
                 <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
                 <div className='flex items-center gap-2'>
-                    <p className='font-semibold text-sm md:text-base'>SHOP</p>
                         <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
-                        <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+                        <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
                 </div>
             </div>
         </div>
