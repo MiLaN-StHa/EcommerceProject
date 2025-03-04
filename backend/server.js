74c65a8a537f6ba93713c97ed7d35efc0ffb7752
Milan <xthamilan88@gmail.com>
2025-03-04 16:46:56 +0545

    api created to add list remove product

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
+import productRouter from './routes/productRoute.js';
 
 // App config
 const app = express();
@@ -21,6 +22,7 @@ app.use(cors());
 
 // API endpoints
 app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
 
 app.get('/', (req, res) => {
   res.send('API Working');
