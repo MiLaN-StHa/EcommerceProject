60f536aff52dffcf45ec439a20667a4b3bebb314
Milan <xthamilan88@gmail.com>
2025-03-05 19:45:58 +0545

    admin panel login page created

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import logo from '../assets/oglogo2.png'
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className=' flex items-center py-2 px-[4%] justify-between'>
       <img className='h-[110px] w-[max(8%,80px)]' src={logo} alt="" />
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm cursor-pointer'>Logout</button>
+      <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm cursor-pointer'>Logout</button>
     </div>
   )
 }
