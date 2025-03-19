baa78c90121fb70961b9417c9f8e964e8e96422b
Milan <xthamilan88@gmail.com>
2025-03-19 16:46:10 +0545

    .

--- backend/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { initiateEsewaPayment, verifyEsewaPayment } from '../controllers/paymentController.js';
-
-const paymentRouter = express.Router();
-
-paymentRouter.post('/esewa/initiate', initiateEsewaPayment);
-paymentRouter.get('/esewa/verify', verifyEsewaPayment);
-
-export default paymentRouter; 
